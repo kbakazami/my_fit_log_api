4f1c2e910b37a76b119fd7217a3ae83c5938fc49
RobinCoblentz <82441417+RobinCoblentz@users.noreply.github.com>
2023-03-01 17:10:53 +0100

    APIRessource impleplementation

--- src/Entity/AdditionalInformation.php
+++ src/Entity/AdditionalInformation.php
@@ -1,16 +1,16 @@
 <?php
 
 namespace App\Entity;
-
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AdditionalInformationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
-#[ORM\Entity(repositoryClass: AdditionalInformationRepository::class)]
 #[ApiResource]
+#[ORM\Entity(repositoryClass: AdditionalInformationRepository::class)]
+
 class AdditionalInformation
 {
     #[ORM\Id]
